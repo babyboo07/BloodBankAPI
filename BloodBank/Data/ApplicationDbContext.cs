1c957c81ffb1d6f5ae6785d47f58e668cb798902
babyboo07 <emma28072002@gmail.com>
2022-07-13 17:15:06 +0700

    update backend

---
+++ BloodBank/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BloodBank.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBank.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<CustomUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Chat> Chats { get; set; }
+
+        public DbSet<Groupblood> Groupbloods { get; set; }
+    }
+}
---
+++ BloodBank/Data/CustomUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BloodBank.Data
+{
+    public class CustomUser:IdentityUser
+    {
+        public int? Sex { get; set; }
+
+        public string Address { get; set; }
+
+        public int? BloodGroupId { get; set; }
+
+        public float? Weight { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
