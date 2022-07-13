1d006b496f73917548f72483bf4571fc80c79f0e
babyboo07 <emma28072002@gmail.com>
2022-07-13 08:39:41 +0700

    update backend

---
+++ BloodBank/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿namespace BloodBank.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime UpDate { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
