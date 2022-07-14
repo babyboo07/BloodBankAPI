295752aa9fceb1a2d005409e0f38de74bda0cfd8
babyboo07 <emma28072002@gmail.com>
2022-07-14 17:46:01 +0700

    new ne

--- BloodBank/Controllers/GroupbloodsController.cs
+++ BloodBank/Controllers/GroupbloodsController.cs
@@ -10,7 +10,7 @@ using BloodBank.Models;
 
 namespace BloodBank.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class GroupbloodsController : ControllerBase
     {
